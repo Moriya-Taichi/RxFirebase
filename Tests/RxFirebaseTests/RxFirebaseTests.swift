d6676fb18ec3f2c06073371b2c53f731917862fc
TaichiMoriya <minecraft0125@gmail.com>
2021-07-19 13:23:03 +0900

    generate swift package

---
+++ Tests/RxFirebaseTests/RxFirebaseTests.swift
@@ -0,0 +1,11 @@
+    import XCTest
+    @testable import RxFirebase
+
+    final class RxFirebaseTests: XCTestCase {
+        func testExample() {
+            // This is an example of a functional test case.
+            // Use XCTAssert and related functions to verify your tests produce the correct
+            // results.
+            XCTAssertEqual(RxFirebase().text, "Hello, World!")
+        }
+    }
